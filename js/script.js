ebc21f24a3e574bb292649349478400ec4d18589
lyamovSergey <lyamofff@gmail.com>
2018-07-26 12:30:27 +0300

    add menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function(){
+	$('.menu__list > li').on('click', function(){
+		$(this).find('img')
+		.attr('src', 'img/lantern-gallery-active1.png');
+		$(this).find('p').addClass('activeItem');
+		$(this).siblings()
+		.find('img')
+		.attr('src', 'img/lantern-menu1.png');
+		$(this).siblings().find('p').removeClass('activeItem');
+		$(this).find('.submenu__list').addClass('activeList').animate({height: 'show'}, 300);;
+		$(this).siblings()
+		.find('.submenu__list').removeClass('activeList').animate({height: 'hide'}, 300);
+	});
 
-		
+
 });
