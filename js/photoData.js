cf6a417206bffe8a6d6e180506104696fe3396bf
lyamovSergey <lyamofff@gmail.com>
2018-07-28 19:56:26 +0300

    fix gallery

---
+++ js/photoData.js
@@ -0,0 +1,29 @@
+var ImageLinks = {
+	banket:{
+		0: 'img/banket/style1.jpg',
+		1: 'img/banket/style2.jpg',
+		2: 'img/banket/style3.jpg',
+		3: 'img/banket/style4.jpg',
+		4: 'img/banket/style5.jpg',
+		5: 'img/banket/style6.jpg',
+		6: 'img/banket/style7.jpg',
+	},
+	vip:{
+		0: 'img/vip/style1.jpg',
+		1: 'img/vip/style2.jpg',
+		2: 'img/vip/style3.jpg',
+		3: 'img/vip/style4.jpg',
+		4: 'img/vip/style5.jpg',
+		5: 'img/vip/style6.jpg',
+		6: 'img/vip/style7.jpg',
+	},
+	main:{
+		0: 'img/main/style1.jpg',
+		1: 'img/main/style2.jpg',
+		2: 'img/main/style3.jpg',
+		3: 'img/main/style4.jpg',
+		4: 'img/main/style5.jpg',
+		5: 'img/main/style6.jpg',
+		6: 'img/main/style7.jpg',
+	}
+};
--- js/script.js
+++ js/script.js
@@ -1,105 +1,134 @@
-$(function(){
-//header links (плавная прокрутка)
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 700);
-    return false;
-});	
-// MENU 
-	//menu animation accordeon
-	var nameCategoriesDefault = $('.submenu__list_active').text();
-	$('.menu .activeList').show();
-	$('.nameCategories').text(nameCategoriesDefault);
-	dataMenu("coctails");
+$(function() {
+	//default
+    dataMenu("coctails");
+    showFoto('banket');
+    //header links (плавная прокрутка)
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 700);
+        return false;
+    });
+
+    // MENU 
+    //menu animation accordeon
+    var nameCategoriesDefault = $('.submenu__list_active').text();
+    $('.menu .activeList').show();
+    $('.nameCategories').text(nameCategoriesDefault);
+    
+
+    $('.menu__list > li').on('click', function() {
+
+        $(this)
+            .find('img')
+            .attr('src', 'img/lantern-gallery-active1.png');
+        $(this)
+            .find('.nameCat')
+            .addClass('activeItem');
+        $(this).siblings()
+            .find('img')
+            .attr('src', 'img/lantern-menu1.png');
+        $(this)
+            .siblings()
+            .find('.nameCat')
+            .removeClass('activeItem');
+        $(this)
+            .find('.submenu__list')
+            .animate({ height: 'show' }, 300);;
+        $(this)
+            .siblings()
+            .find('.submenu__list')
+            .animate({ height: 'hide' }, 300);
+    });
+
+    function dataMenu(id) {
+        $('.description__block').remove();
+        jQuery.each(menuData, function(i, val) { //перебор объекта с данными
+            var leng = 0; //длина объекта
+            if (i == id) { //активен определенный объект, он же ключ к данным
+                for (var key in val) { //вычислить длину объекта
+                    leng++;
+                };
+                for (var i = 1; i <= leng; i++) { //добавить необходимое количество данных в HTML
+
+                    $('.menu__description')
+                        .append('<div class="description__block"><div class="wrapper"><h1>' + val[i].name + '</h1><p>' + val[i].description + '</p></div><p class="menu__description__vol">' + val[i].vol + '</p><p class="menu__description__price">' + val[i].price + '</p></div>');
+                }
+            }
+        });
+    }
 
 
-	$('.menu__list > li').on('click', function(){
-
-		$(this)
-			.find('img')
-			.attr('src', 'img/lantern-gallery-active1.png');
-		$(this)
-			.find('.nameCat')
-			.addClass('activeItem');
-		$(this).siblings()
-			.find('img')
-			.attr('src', 'img/lantern-menu1.png');
-		$(this)
-			.siblings()
-			.find('.nameCat')
-			.removeClass('activeItem');
-		$(this)
-			.find('.submenu__list')
-			.animate({height: 'show'}, 300);;
-		$(this)
-			.siblings()
-			.find('.submenu__list')
-			.animate({height: 'hide'}, 300);
-	});
-	function dataMenu(id){
-		$('.description__block').remove();
-		jQuery.each(menuData, function(i, val){ //перебор объекта с данными
-			var leng = 0;                       //длина объекта
-			if( i == id){						//активен определенный объект, он же ключ к данным
-				for( var key in val ){			//вычислить длину объекта
-					leng++;
-				};
-				for( var i = 1; i <= leng; i++ ){ //добавить необходимое количество данных в HTML
-
-					$('.menu__description')
-					.append('<div class="description__block"><div class="wrapper"><h1>' +val[i].name+ '</h1><p>'+val[i].description+'</p></div><p class="menu__description__vol">' + val[i].vol + '</p><p class="menu__description__price">' + val[i].price + '</p></div>');
-				}
-			}
-		});
-	}
+    //menu data
+    $('.submenu__list li').on('click', function() {
+        var noActiveLists = $(this).parents()[1];
+        var id = $(this).find('p').attr('id');
+        $(this).find('p')
+            .addClass('submenu__list_active')
+            .parent()
+            .siblings()
+            .find('p')
+            .removeClass('submenu__list_active');
+        $(noActiveLists).siblings()
+            .find('p')
+            .removeClass('submenu__list_active');
+        var nameCategories = $(this)
+            .find('p')
+            .text();
+        $('.nameCategories').text(nameCategories);
+        dataMenu(id);
+    });
 
 
-	//menu data
-	$('.submenu__list li').on('click', function(){
-		var noActiveLists = $(this).parents()[1];
-		var id = $(this).find('p').attr('id');
-		$(this).find('p')
-			.addClass('submenu__list_active')
-			.parent()
-			.siblings()
-			.find('p')
-			.removeClass('submenu__list_active');
-		$(noActiveLists).siblings()
-			.find('p')
-			.removeClass('submenu__list_active');
-		var nameCategories = $(this)
-			.find('p')
-			.text();
-		$('.nameCategories').text(nameCategories);
-		dataMenu(id);
-	});
+    //Gallery block
+    //gallery-nav lantern
+    $('.gallery-nav__item').on('click', function() {
+        var id = $(this).attr('id');
+        var img = $(this)
+            .find('img')
+            .attr('src');
+        if (img == "img/lantern-gallery.png") {
+            $(this)
+                .find('img')
+                .attr('src', 'img/lantern-gallery-active1.png');
+            $(this)
+                .siblings()
+                .find('img')
+                .attr('src', 'img/lantern-gallery.png');
+        }
+        showFoto(id);
 
+    });
+	        //Gallery add foto
+	function showFoto(id) {
+	    jQuery.each(ImageLinks, function(x, val) {
+	        if (x == id) {
+	            var obj = ImageLinks[x];
+	            var links = $.each(obj, function(i, val) {
+	                return val;
+	            });
+	            $('.photoBlock__item').find('a').each(function(index, val) {
+	                var link = links[index];
+	                $(val).attr('href', link);
+	            });
 
-//Gallery block
-	//gallery-nav lantern
-	$('.gallery-nav__item').on('click', function(){
-		var id = $(this).attr('id');
-		$('.photoBlock__item a').attr('href', )
-		
-		var img = $(this)
-			.find('img')
-			.attr('src');
-		if( img == "img/lantern-gallery.png" ){
-			$(this)
-				.find('img')
-				.attr('src', 'img/lantern-gallery-active1.png');
-			$(this)
-				.siblings()
-				.find('img')
-				.attr('src', 'img/lantern-gallery.png');	
-		}
-	});
+	            $('.photoBlock__item').find('img').each(function(index, val) {
+	                var link = links[index];
+	                $(val).attr('src', link);
+	            });
+	        }
+	  //       $('#bestgallerys').photobox('a',{ //gallery plugin
+			//     time:0,
+			//     thumbs:true,
+			//     zoomable:true
+			// });
+	    });
+	};
 
-	//photo init
-	$('.photoBlock').masonry({
-  		columnWidth: '.photoBlock__item',
-  		itemSelector: '.photoBlock__item'
-	});
+    //photo init
+    $('.photoBlock').masonry({
+        columnWidth: '.photoBlock__item',
+        itemSelector: '.photoBlock__item'
+    });
 });
